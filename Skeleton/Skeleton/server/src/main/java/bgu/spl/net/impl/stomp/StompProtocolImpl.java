f97bb8ad5e21e6b5d42a431aaa5d4306e1781b3e
Levi1308 <akivalev@post.bgu.ac.il>
2025-01-08 09:39:57 +0000

    createStompProtocolImpl

--- Skeleton/Skeleton/server/src/main/java/bgu/spl/net/impl/stomp/StompProtocolImpl.java
+++ Skeleton/Skeleton/server/src/main/java/bgu/spl/net/impl/stomp/StompProtocolImpl.java
@@ -1,5 +1,28 @@
 package bgu.spl.net.impl.stomp;
 
-public class StompProtocolImpl {
+import bgu.spl.net.api.StompMessagingProtocol;
+import bgu.spl.net.srv.Connections;
+
+public class StompProtocolImpl  implements StompMessagingProtocol<String> {
+    
+
+
+    @Override
+    public void start (int connectionId, Connections<String> connections){
+
+    }
+
+    @Override
+    public void process(String message) {
+
+    }
+
+    @Override
+    public boolean shouldTerminate() {
+       return false;
+    }
+
+
+
 
 }
